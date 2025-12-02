c3c3c22070ca7dc068de059b59d8f06479723f5b
Vignesh Varma <seeguvigneswarvarma@gmail.com>
2025-12-02 00:08:25 -0500

    Fix: only use check_same_thread for SQLite in test_full_coverage.py

--- tests/test_full_coverage.py
+++ tests/test_full_coverage.py
@@ -24,8 +24,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
+
 TEST_DATABASE_URL = os.getenv("TEST_DATABASE_URL", "sqlite:///./test_coverage.db")
-engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+if TEST_DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(TEST_DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(TEST_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
